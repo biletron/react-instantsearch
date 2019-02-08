d88308740856fc8fc2dc662d1617900e9f4fe01d
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-02-08 12:20:22 +0100

    feat(DOMMaps): expose withGoogleMaps

---
+++ packages/react-instantsearch-dom-maps/src/withGoogleMaps.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
+type Subtract<T, K> = Omit<T, keyof K>;
+
+export interface GoogleMapsContext {
+  __ais_geo_search__google_maps__: {
+    google: typeof google;
+    instance: google.maps.Map;
+  };
+}
+
+export interface WithGoogleMapsProps {
+  google: typeof google;
+  googleMapsInstance: google.maps.Map;
+}
+
+const withGoogleMaps = <P extends WithGoogleMapsProps>(
+  Wrapped: React.ComponentType<P>
+) => {
+  const WithGoogleMaps: React.FC<Subtract<P, WithGoogleMapsProps>> = (
+    props,
+    context: GoogleMapsContext
+  ) => {
+    const { google, instance } = context.__ais_geo_search__google_maps__;
+
+    return (
+      <Wrapped
+        // @TODO: remove the cast once TypeScript fixes the issue
+        // https://github.com/Microsoft/TypeScript/issues/28938
+        {...props as P}
+        google={google}
+        googleMapsInstance={instance}
+      />
+    );
+  };
+
+  WithGoogleMaps.contextTypes = {
+    __ais_geo_search__google_maps__: PropTypes.shape({
+      google: PropTypes.object,
+      instance: PropTypes.object,
+    }),
+  };
+
+  return WithGoogleMaps;
+};
+
+export default withGoogleMaps;
